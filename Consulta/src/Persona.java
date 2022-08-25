7381b4dd7c8fd7f5eac04f594063bf9535c9b60b
ramimagnatera <ramiro.magnatera@gmail.com>
2022-08-24 20:24:18 -0400

    Creación clase Persona

---
+++ Consulta/src/Persona.java
@@ -0,0 +1,65 @@
+public class Persona {
+    // Declaracion de variables
+    private String Nombre, Genero;
+    private int id, rut, edad, numero;
+
+    //Constructor
+    Persona(){
+        Nombre = "";
+        Genero = "";
+        id = 0;
+        rut = 0;
+        edad = 0;
+        numero = 0;
+    }
+
+    // Getter y Setter
+    public int GetId() {
+        return id;
+    }
+
+    public void SetId(int id) {
+        this.id = id;
+    }
+
+    public String GetNombre() {
+        return Nombre;
+    }
+
+    public void SetNombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public String GetGenero() {
+        return Genero;
+    }
+
+    public void SetGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public int GetRut() {
+        return rut;
+    }
+
+    public void SetRut(int rut) {
+        this.rut = rut;
+    }
+
+    public int GetEdad() {
+        return edad;
+    }
+
+    public void SetEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int GetNumero() {
+        return numero;
+    }
+
+    public void SetNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
--- Consulta/src/index.java
+++ Consulta/src/index.java
@@ -1,9 +1,7 @@
 
 public class index {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
-
 }
